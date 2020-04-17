af59bd254db83cbdf096729a27ace7b5773114ca
Kuchar <jkuchar@mail.ee>
2020-04-17 21:44:16 +0200

    Pocztek komentarzy

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget = forms.Textarea)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), max_length=25)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': "form-control", "placeholder": "Wprowadź email"}))
+    to = forms.EmailField(widget=forms.EmailInput(attrs={'class': "form-control", "placeholder": "Wprowadź email"}))
+    comments = forms.CharField(required=False, widget = forms.Textarea(attrs={"class": "form-control","rows": 3}))
     
