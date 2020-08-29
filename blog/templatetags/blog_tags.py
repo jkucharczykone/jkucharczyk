a316f67b69bdccc8321276170826df1bbc72c190
Kuchar <jkuchar@mail.ee>
2020-08-29 15:58:59 +0200

    Dodano func najczesciej komentowane posty

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.db.models import Count
 register = template.Library()
 
 from ..models import Post
@@ -10,4 +10,9 @@ def total_posts():
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.objects.all().filter(status="published").order_by('-publish')[:count]
-    return {'latest_posts': latest_posts}
+    return {'latest_posts': latest_posts, 'title': "ostatnie posty: "}
+
+@register.inclusion_tag('blog/post/most_commented.html')
+def show_most_commented_posts(count=5):
+    most_commented_posts = Post.objects.all().filter(status="published").annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts, 'title': "najczęściej komentowane posty: "}
